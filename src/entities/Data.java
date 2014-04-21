7537c92ae026a2765e3b7d87b4e66e64c064332b
wengia <wengia_mawenjia@hotmail.com>
2014-04-20 18:54:48 -0700

    add entities

---
+++ src/entities/Data.java
@@ -0,0 +1,55 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Data implements Serializable {
+	private long id;
+	private Date time;
+	private int bg;
+	private int insulin;
+	private long userId;
+	private int chatId;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public Date getTime() {
+		return time;
+	}
+	public void setTime(Date time) {
+		this.time = time;
+	}
+	public int getBg() {
+		return bg;
+	}
+	public void setBg(int bg) {
+		this.bg = bg;
+	}
+	public int getInsulin() {
+		return insulin;
+	}
+	public void setInsulin(int insulin) {
+		this.insulin = insulin;
+	}
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public int getChatId() {
+		return chatId;
+	}
+	public void setChatId(int chatId) {
+		this.chatId = chatId;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/entities/User.java
@@ -0,0 +1,84 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+
+public class User implements Serializable {
+	private long id;
+	private String email;
+	private String username;
+	private String password;
+	private String confirmPwd;
+	private String phoneNo;
+	private Date dateOfBirth;
+	private String gender;
+	private String role;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmPwd() {
+		return confirmPwd;
+	}
+	public void setConfirmPwd(String confirmPwd) {
+		this.confirmPwd = confirmPwd;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	public boolean validate() {
+		if(email == null || username == null || password == null || role == null)
+			return false;
+		if(confirmPwd == null || !password.equals(confirmPwd))
+			return false;
+		
+		return true;
+	}
+	
+	
+
+}
